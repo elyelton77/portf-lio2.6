83d9db081e727c63a66161fc918d9689e7128e26
Elyelton Afonso <ellyeltonafonso.14@gmail.com>
2025-12-30 18:46:37 -0300

    Update GitHub link for portf-lio2.6

--- projetos.js
+++ projetos.js
@@ -13,7 +13,7 @@ const projetos = {
             "Otimizado para performance"
         ],
         demoLink: "#",
-        githubLink: "https://github.com/elyelton77/portf-lio2.6.git"
+        githubLink: "https://github.com/elyelton77/portf-lio2.6"
     },
     Loja_de_Roupas: {
         titulo: "Loja de Roupas",
