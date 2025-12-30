89179a229d0029134b9bfdc9ee26778fa74cccf9
Elyelton Afonso <ellyeltonafonso.14@gmail.com>
2025-12-30 18:44:09 -0300

    Update GitHub link in projetos.js

--- projetos.js
+++ projetos.js
@@ -13,7 +13,7 @@ const projetos = {
             "Otimizado para performance"
         ],
         demoLink: "#",
-        githubLink: "https://github.com/elyelton77/portfolio2.0"
+        githubLink: "https://github.com/elyelton77/portf-lio2.6.git"
     },
     Loja_de_Roupas: {
         titulo: "Loja de Roupas",
